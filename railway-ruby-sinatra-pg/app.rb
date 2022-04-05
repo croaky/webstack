8977e021588b352cbd44debe12db18b9829a4ab9
Dan Croak <dan@statusok.com>
2022-04-04 17:10:26 -0700

    railway-ruby-sinatra-pg: configure Postgres

--- railway-ruby-sinatra-pg/app.rb
+++ railway-ruby-sinatra-pg/app.rb
@@ -1,6 +1,15 @@
+require "rubygems"
+require "bundler"
+
+Bundler.setup(:default)
+
+require "pg"
 require "sinatra"
 
+db = PG.connect(ENV.fetch("DATABASE_URL"))
+
 get "/" do
-  content_type :json
+  db.exec "SELECT 1"
+  content_type "application/json"
   {status: "ok"}.to_json
 end
