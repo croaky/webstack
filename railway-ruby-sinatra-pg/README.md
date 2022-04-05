76478a128b26343ecb3893c1a93ec1b9a801716c
Dan Croak <dan@statusok.com>
2022-04-04 17:24:38 -0700

    railway-ruby-sinatra-pg: add Puma config

--- railway-ruby-sinatra-pg/README.md
+++ railway-ruby-sinatra-pg/README.md
@@ -3,3 +3,16 @@
 Go to <https://railway.app/new>.
 Click "Deploy from Repo".
 Select this repo.
+Select `main` branch.
+Click "Deploy".
+
+It will fail.
+
+Click "+ New" button.
+Click "Database".
+Click "Add PostgreSQL".
+
+Click the original web service.
+Click "Settings".
+Set "Root directory" to `/railway-ruby-sinatra-pg`.
+Set "Healthcheck Path" to `/`.
---
+++ railway-ruby-sinatra-pg/config/puma.rb
@@ -0,0 +1,14 @@
+# Increasing the number of workers will increase resting memory.
+# Increasing the number of threads will increase potential bloat
+# when processes are responding to heavy requests.
+#
+# A low number of workers and threads provides adequate
+# performance for most apps, even under load,
+# while maintaining a low risk of overusing memory.
+workers ENV.fetch("PUMA_WORKERS", 2).to_i
+threads_count = ENV.fetch("PUMA_THREADS", 2).to_i
+threads(threads_count, threads_count)
+
+preload_app!
+
+environment ENV.fetch("APP_ENV", "dev")
