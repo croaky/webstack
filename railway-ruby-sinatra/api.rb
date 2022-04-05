7e875be7b7859817c637777c2ef6112a21b141b3
Dan Croak <dan@statusok.com>
2022-04-05 12:43:53 -0700

    railway-ruby-sinatra: ping Postgres

--- railway-ruby-sinatra/api.rb
+++ railway-ruby-sinatra/api.rb
@@ -1,6 +1,21 @@
+require "pg"
 require "sinatra"
 
+ENV["APP_ENV"] ||= "dev"
+
+path = "#{__dir__}/env/#{ENV.fetch("APP_ENV")}"
+
+if File.exist?(path)
+  File.readlines(path).each do |line|
+    key, val = line.split("=", 2)
+    ENV[key] = val.chomp
+  end
+end
+
+db = PG.connect(ENV.fetch("DATABASE_URL"))
+
 get "/" do
+  db.exec "SELECT 1"
   content_type :json
   {status: "ok"}.to_json
 end
