78986431910acae4f5b61e69b7df898b0a958e99
Dan Croak <dan@statusok.com>
2022-04-05 22:13:43 -0700

    railway-node-express: add

---
+++ railway-node-express/api.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.json({ status: "ok" })
+})
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`)
+})
