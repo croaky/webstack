e1c4f79356840df9cdfaa217d9fc5fb9487ea478
Dan Croak <dan@statusok.com>
2022-04-07 21:49:06 -0700

    vercel-go: ping Postgres
    https://github.com/croaky/webstack/pull/10

--- vercel-go/api/index.go
+++ vercel-go/api/index.go
@@ -1,11 +1,20 @@
 package api
 
 import (
+	"database/sql"
 	"fmt"
+	"log"
 	"net/http"
+	"os"
 )
 
 func Handler(w http.ResponseWriter, r *http.Request) {
+	dbUrl := os.Getenv("DATABASE_URL")
+	db, err := sql.Open("postgres", dbUrl)
+	if err != nil {
+		log.Fatal(err)
+	}
+	db.Query("SELECT 1")
 	w.Header().Set("Content-Type", "application/json")
 	fmt.Fprintf(w, "{status:\"ok\"}")
 }
