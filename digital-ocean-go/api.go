a82af7ba0095692059dada1dff84965564fbfe63
Dan Croak <dan@statusok.com>
2022-09-23 13:25:56 -0400

    digital-ocean-go: add

---
+++ digital-ocean-go/api.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	_ "github.com/lib/pq"
+)
+
+func main() {
+	// env
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	dbUrl := os.Getenv("DATABASE_URL")
+	if dbUrl == "" {
+		dbUrl = "postgres:///webstack_dev"
+	}
+
+	// db
+	db, err := sql.Open("postgres", dbUrl)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// routes
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		db.Query("SELECT 1")
+		w.Header().Set("Content-Type", "application/json")
+		fmt.Fprintf(w, "{\"status\":\"ok\"}")
+	})
+
+	// listen
+	log.Println("Listening at http://localhost:" + port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
