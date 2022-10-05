a008e2ed9457616e87f189ffb130aeb6b4f997a6
Dan Croak <dan@statusok.com>
2022-10-05 17:17:00 -0400

    railway-go: tweak
    * Rename `api.go` to `main.go`.
    * Extract handler into own file.
    * Add `GET` as allowed HTTP method.
    * Use `os.LookupEnv` for clarity.
    
    https://github.com/croaky/webstack/pull/37

--- railway-go/env/env.go
+++ railway-go/env/env.go
@@ -8,8 +8,8 @@ import (
 // String returns the value of the named environment variable.
 // If name isn't in the environment or is empty, it returns fallback.
 func String(name string, fallback string) string {
-	value := os.Getenv(name)
-	if value == "" {
+	value, ok := os.LookupEnv(name)
+	if !ok {
 		value = fallback
 	}
 	return value
