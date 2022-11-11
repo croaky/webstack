711ac174dcacd7c6257754a407ca83e36f1e5e83
Dan Croak <dan@statusok.com>
2022-11-10 20:15:25 -0500

    render-go: use net/http instead of gin

--- render-go/go.mod
+++ render-go/go.mod
@@ -3,17 +3,11 @@ module server
 go 1.19
 
 require (
-	github.com/gin-gonic/gin v1.8.1
 	github.com/jackc/pgx/v4 v4.16.0
 	github.com/lib/pq v1.10.4 // indirect
 )
 
 require (
-	github.com/gin-contrib/sse v0.1.0 // indirect
-	github.com/go-playground/locales v0.14.0 // indirect
-	github.com/go-playground/universal-translator v0.18.0 // indirect
-	github.com/go-playground/validator/v10 v10.11.1 // indirect
-	github.com/goccy/go-json v0.9.11 // indirect
 	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
 	github.com/jackc/pgconn v1.12.0 // indirect
 	github.com/jackc/pgio v1.0.0 // indirect
@@ -22,17 +16,7 @@ require (
 	github.com/jackc/pgservicefile v0.0.0-20200714003250-2b9c44734f2b // indirect
 	github.com/jackc/pgtype v1.11.0 // indirect
 	github.com/jackc/puddle v1.2.1 // indirect
-	github.com/json-iterator/go v1.1.12 // indirect
-	github.com/leodido/go-urn v1.2.1 // indirect
-	github.com/mattn/go-isatty v0.0.16 // indirect
-	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
-	github.com/modern-go/reflect2 v1.0.2 // indirect
-	github.com/pelletier/go-toml/v2 v2.0.5 // indirect
-	github.com/ugorji/go/codec v1.2.7 // indirect
+	github.com/stretchr/testify v1.8.0 // indirect
 	golang.org/x/crypto v0.0.0-20220926161630-eccd6366d1be // indirect
-	golang.org/x/net v0.0.0-20220927171203-f486391704dc // indirect
-	golang.org/x/sys v0.0.0-20220928140112-f11e5e49a4ec // indirect
 	golang.org/x/text v0.3.7 // indirect
-	google.golang.org/protobuf v1.28.1 // indirect
-	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
--- render-go/main.go
+++ render-go/main.go
@@ -4,85 +4,39 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"net/http"
 	"os"
 
-	"github.com/gin-gonic/gin"
 	"github.com/jackc/pgx/v4/pgxpool"
 )
 
-func env(key string, fallback string) string {
-	val, ok := os.LookupEnv(key)
-	if !ok {
-		val = fallback
-	}
-	return val
-}
-
-type Server struct {
-	db *pgxpool.Pool
-	rt *gin.Engine
-}
-
-func NewServer(ctx context.Context, db *pgxpool.Pool) *Server {
-	gin.SetMode(gin.ReleaseMode)
-	rt := gin.New()
-	rt.UseH2C = true
-
-	// middleware
-	rt.Use(gin.LoggerWithFormatter(func(param gin.LogFormatterParams) string {
-		return fmt.Sprintf("%s %d %s %s\n",
-			fmt.Sprintf("% 4dms", param.Latency.Milliseconds()),
-			param.StatusCode,
-			param.Method,
-			param.Path,
-			// TODO: req_id, git_sha
-		)
-	}))
-	rt.Use(gin.Recovery())
-	rt.Use(func(c *gin.Context) {
-		c.Header("Accept", "application/json")
-		c.Header("Access-Control-Allow-Methods", "OPTIONS,GET,POST")
-		c.Header("Content-Type", "application/json")
-		c.Header("X-Content-Type-Options", "nosniff")
-		c.Header("X-Frame-Options", "DENY")
-		c.Header("X-XSS-Protection", "1; mode=block")
-	})
-
-	// OPTIONS catch-all for pre-flight requests
-	rt.OPTIONS("/*all", func(c *gin.Context) {
-		c.String(204, "")
-	})
-
-	// routes
-	rt.GET("/", func(c *gin.Context) {
-		var col int
-		db.QueryRow(ctx, "SELECT 1").Scan(&col)
-		c.JSON(200, gin.H{"status": "ok"})
-	})
-
-	return &Server{
-		db: db,
-		rt: rt,
-	}
-}
-
 func main() {
-	ctx := context.Background()
-
 	// env
-	port := env("PORT", "8080")
-	dbUrl := env("DATABASE_URL", "postgres:///webstack_dev")
+	port, ok := os.LookupEnv("PORT")
+	if !ok {
+		port = "8080"
+	}
+	dbUrl, ok := os.LookupEnv("DATABASE_URL")
+	if !ok {
+		dbUrl = "postgres:///webstack_dev"
+	}
 
 	// db
-	db, err := pgxpool.Connect(ctx, dbUrl)
+	db, err := pgxpool.Connect(context.Background(), dbUrl)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer db.Close()
 
-	// server
-	s := NewServer(ctx, db)
+	// routes
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		var col int
+		db.QueryRow(r.Context(), "SELECT 1").Scan(&col)
+		w.Header().Set("Content-Type", "application/json")
+		fmt.Fprintf(w, "{\"status\":\"ok\"}")
+	})
 
+	// listen
 	log.Println("Listening at http://localhost:" + port)
-	s.rt.Run(":" + port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
