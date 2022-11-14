d64e6b5f69806db277a32879213d204b703f11cc
Dan Croak <dan@statusok.com>
2022-11-13 20:07:38 -0500

    docs: move fly-go-cockroach back to live
    It is performing well and has similarities to Neon,
    which I'm still evaluating.

--- docs/main.ts
+++ docs/main.ts
@@ -28,7 +28,6 @@ for (let i = 0; i < data.length; i++) {
 }
 
 // add historical data for apps I've shut down to save money
-sorted.push(["fly-go-cockroach", (100.0).toFixed(2), 261, 966]);
 sorted.push(["fly-go-planetscale", 99.99, 207, 523]);
 sorted.push(["fly-go-postgres-read-replicas", 99.99, 115, 434]);
 sorted.push(["fly-go-sqlite", 99.99, 125, 392]);
