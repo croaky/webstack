17e14c0cd95c9577ab836448d1b0bf86c601cd55
Dan Croak <dan@statusok.com>
2022-11-20 10:22:29 -0500

    fly-go-crunchy-replicas: use FLY_ALLOC_ID in Tailscale machine name
    https://fly.io/docs/reference/runtime-environment/

--- fly-go-crunchy-replicas/start.sh
+++ fly-go-crunchy-replicas/start.sh
@@ -1,14 +1,10 @@
 #!/bin/sh
 
-# Connect machine to Tailscale tailnet.
-/app/tailscaled --state=/var/lib/tailscale/tailscaled.state --socket=/var/run/tailscale/tailscaled.sock &
-/app/tailscale up --authkey="${TAILSCALE_AUTHKEY}" --hostname="fly-${FLY_REGION}"
-
-# Auto-remove immediately when machine shuts down.
+# Connect machine to Tailscale tailnet and auto-remove immediately when machine shuts down.
 # https://tailscale.com/blog/ephemeral-logout/
-# Instead of a 30m-45h window.
-# https://tailscale.com/kb/1111/ephemeral-nodes/
-/app/tailscale logout --authkey="${TAILSCALE_AUTHKEY}"
+/app/tailscaled -state=mem: --socket=/var/run/tailscale/tailscaled.sock &
+ID=$(echo "${FLY_ALLOC_ID}" | cut -d '-' -f 1)
+/app/tailscale up --authkey="${TAILSCALE_AUTHKEY}" --hostname="fly-${FLY_REGION}-${ID}"
 
 # Run server
 /app/server
