deace3e0628f57f1392247e5472f69c9aef53c18
Dan Croak <dan@statusok.com>
2022-11-20 18:53:32 -0500

    railway-go-crunchy-bridge: suffix each machine w/ Git SHA

--- railway-go-crunchy-bridge/start.sh
+++ railway-go-crunchy-bridge/start.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 # Connect machine to Tailscale tailnet
-/app/tailscaled --tun=userspace-networking --socks5-server="localhost:1055" &
-/app/tailscale up --authkey="${TAILSCALE_AUTHKEY}" --hostname="railway-app"
-echo "Tailscale started"
+/app/tailscaled --state=mem: --tun=userspace-networking --socks5-server="localhost:1055" &
+ID="${RAILWAY_GIT_COMMIT_SHA}:0:8"
+/app/tailscale up --authkey="${TAILSCALE_AUTHKEY}" --hostname="railway-app-${ID}"
 
 # Run server
 ALL_PROXY="socks5://localhost:1055/" /app/server
