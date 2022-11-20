ef1a05323ae25a5a21c56dad5712016910facaaa
Dan Croak <dan@statusok.com>
2022-11-20 09:34:46 -0500

    fly-go-crunchy-replicas: use Tailscale to connect to database
    Useful reading:
    https://tailscale.com/kb/1132/flydotio/

---
+++ fly-go-crunchy-replicas/start.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# Connect machine to Tailscale tailnet.
+/app/tailscaled --state=/var/lib/tailscale/tailscaled.state --socket=/var/run/tailscale/tailscaled.sock &
+/app/tailscale up --authkey="${TAILSCALE_AUTHKEY}" --hostname=fly-app
+
+# Auto-remove immediately when machine shuts down.
+# https://tailscale.com/blog/ephemeral-logout/
+# Instead of a 30m-45h window.
+# https://tailscale.com/kb/1111/ephemeral-nodes/
+/app/tailscale logout --authkey="${TAILSCALE_AUTHKEY}"
+
+# Run server
+/app/server
