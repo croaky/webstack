2373bfd0262bd54337b31183a52e8b9902edab14
Dan Croak <dan@statusok.com>
2022-11-20 18:56:01 -0500

    railway-go-crunchy-bridge: fix restart, SHA length

--- railway-go-crunchy-bridge/start.sh
+++ railway-go-crunchy-bridge/start.sh
@@ -2,7 +2,7 @@
 
 # Connect machine to Tailscale tailnet
 /app/tailscaled --state=mem: --tun=userspace-networking --socks5-server="localhost:1055" &
-ID="${RAILWAY_GIT_COMMIT_SHA}:0:8"
+ID="${RAILWAY_GIT_COMMIT_SHA:0:8}"
 /app/tailscale up --authkey="${TAILSCALE_AUTHKEY}" --hostname="railway-app-${ID}"
 
 # Run server
