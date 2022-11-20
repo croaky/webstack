6e23026382f7a0075c9605a6bbadf48f3359ff3f
Dan Croak <dan@statusok.com>
2022-11-20 15:42:56 -0500

    fly-go-crunchy-replicas: fix tailscaled --state flag

--- fly-go-crunchy-replicas/start.sh
+++ fly-go-crunchy-replicas/start.sh
@@ -2,7 +2,7 @@
 
 # Connect machine to Tailscale tailnet and auto-remove immediately when machine shuts down.
 # https://tailscale.com/blog/ephemeral-logout/
-/app/tailscaled -state=mem: --socket=/var/run/tailscale/tailscaled.sock &
+/app/tailscaled --state=mem: --socket=/var/run/tailscale/tailscaled.sock &
 ID=$(echo "${FLY_ALLOC_ID}" | cut -d '-' -f 1)
 /app/tailscale up --authkey="${TAILSCALE_AUTHKEY}" --hostname="fly-${FLY_REGION}-${ID}"
 
