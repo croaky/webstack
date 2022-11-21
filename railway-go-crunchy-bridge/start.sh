e9478a5b017efd58db49b8ea7c553121fa03952e
Dan Croak <dan@statusok.com>
2022-11-20 22:23:11 -0500

    until, curl

--- railway-go-crunchy-bridge/start.sh
+++ railway-go-crunchy-bridge/start.sh
@@ -4,7 +4,11 @@ HOST="railway-app-${RAILWAY_GIT_COMMIT_SHA:0:8}"
 
 # Connect machine to Tailscale tailnet
 /app/tailscaled --tun=userspace-networking --socks5-server="localhost:1055" &
-/app/tailscale up --authkey="$TAILSCALE_AUTHKEY" --hostname="$HOST"
+
+until /app/tailscale up --authkey="$TAILSCALE_AUTHKEY" --hostname="$HOST"
+do
+  sleep 0.1
+done
 
 # Run app
 ALL_PROXY="socks5://localhost:1055/" /app/server
