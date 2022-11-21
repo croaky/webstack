769274798e68307cd5191db59804735ba221091c
Dan Croak <dan@statusok.com>
2022-11-20 19:25:41 -0500

    railway-go-crunchy-bridge: try Fly.io docs for Tailscale
    https://tailscale.com/kb/1132/flydotio/

--- railway-go-crunchy-bridge/start.sh
+++ railway-go-crunchy-bridge/start.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 # Connect machine to Tailscale tailnet
-/app/tailscaled --state=mem: --tun=userspace-networking --socks5-server="localhost:1055" &
+/app/tailscaled --state=mem: --socket=/var/run/tailscale/tailscaled.sock &
 ID="${RAILWAY_GIT_COMMIT_SHA:0:8}"
 /app/tailscale up --authkey="${TAILSCALE_AUTHKEY}" --hostname="railway-app-${ID}"
 
 # Run server
-ALL_PROXY="socks5://localhost:1055/" /app/server
+/app/server
