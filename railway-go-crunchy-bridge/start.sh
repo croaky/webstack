19a5554493ec201f7f71003d3a003f935b0e8d28
Dan Croak <dan@statusok.com>
2022-11-20 22:12:32 -0500

    back to Heroku version

--- railway-go-crunchy-bridge/start.sh
+++ railway-go-crunchy-bridge/start.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 
 HOST="railway-app-${RAILWAY_GIT_COMMIT_SHA:0:8}"
-SOCKET="$HOME/tailscale-storage/tailscale.sock"
 
 # Connect machine to Tailscale tailnet
-/app/tailscaled --state=mem: --tun=userspace-networking --socks5-server="localhost:1055" --socket="$SOCKET" &
-/app/tailscale --socket="$SOCKET" up --authkey="$TAILSCALE_AUTHKEY" --hostname="$HOST"
+/app/tailscaled --tun=userspace-networking --socks5-server="localhost:1055" &
+/app/tailscale up --authkey="$TAILSCALE_AUTHKEY" --hostname="$HOST"
 
-# Run server with 'exec' to forward SIGINT & SIGTERM to program for cleanup.
+# Run app
 ALL_PROXY="socks5://localhost:1055/" /app/server
