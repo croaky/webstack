5442f42bcdd20bdce9784280e82d3aaaba4f98c6
Dan Croak <dan@statusok.com>
2022-11-20 21:07:31 -0500

    don't run as root user

--- railway-go-crunchy-bridge/start.sh
+++ railway-go-crunchy-bridge/start.sh
@@ -1,14 +1,11 @@
 #!/bin/sh
 
-# Connect machine to Tailscale tailnet
-/app/tailscaled --state=mem: --tun=userspace-networking --socks5-server="localhost:1055" &
-ID="${RAILWAY_GIT_COMMIT_SHA:0:8}"
+HOST="railway-app-${RAILWAY_GIT_COMMIT_SHA:0:8}"
+SOCKET="$HOME/tailscale-storage/tailscale.sock"
 
-until /app/tailscale up --authkey="${TAILSCALE_AUTHKEY}" --hostname="railway-app-${ID}"
-do
-  echo "Waiting for Tailscale auth..."
-  sleep 5
-done
+# Connect machine to Tailscale tailnet
+/app/tailscaled --state=mem: --tun=userspace-networking --socks5-server="localhost:1055" --socket="$SOCKET" &
+/app/tailscale up --authkey="$TAILSCALE_AUTHKEY" --hostname="$HOST" --socket="$SOCKET"
 
 # Run server
 ALL_PROXY="socks5://localhost:1055/" /app/server
