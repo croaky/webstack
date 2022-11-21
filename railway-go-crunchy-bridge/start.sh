2df4d85be91e9254b8b42ebd5fe99fe2274bb59c
Dan Croak <dan@statusok.com>
2022-11-20 20:59:39 -0500

    wait for Tailscale auth

--- railway-go-crunchy-bridge/start.sh
+++ railway-go-crunchy-bridge/start.sh
@@ -3,7 +3,12 @@
 # Connect machine to Tailscale tailnet
 /app/tailscaled --state=mem: --tun=userspace-networking --socks5-server="localhost:1055" &
 ID="${RAILWAY_GIT_COMMIT_SHA:0:8}"
-/app/tailscale up --authkey="${TAILSCALE_AUTHKEY}" --hostname="railway-app-${ID}"
+
+until /app/tailscale up --authkey="${TAILSCALE_AUTHKEY}" --hostname="railway-app-${ID}"
+do
+  echo "Waiting for Tailscale auth..."
+  sleep 5
+done
 
 # Run server
 ALL_PROXY="socks5://localhost:1055/" /app/server
