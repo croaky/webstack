651c29446769d505d7a4bfbc55cd84ec63cc5fe9
Dan Croak <dan@statusok.com>
2022-11-20 21:14:37 -0500

    try again

--- railway-go-crunchy-bridge/start.sh
+++ railway-go-crunchy-bridge/start.sh
@@ -5,7 +5,12 @@ SOCKET="$HOME/tailscale-storage/tailscale.sock"
 
 # Connect machine to Tailscale tailnet
 /app/tailscaled --state=mem: --tun=userspace-networking --socks5-server="localhost:1055" --socket="$SOCKET" &
-/app/tailscale up --authkey="$TAILSCALE_AUTHKEY" --hostname="$HOST" --socket="$SOCKET"
 
-# Run server
-ALL_PROXY="socks5://localhost:1055/" /app/server
+until /app/tailscale up --authkey="$TAILSCALE_AUTHKEY" --hostname="$HOST" --socket="$SOCKET"
+do
+  echo "Waiting for Tailscale auth"
+  sleep 5
+done
+
+# Run server with 'exec' to forward SIGINT & SIGTERM to program for cleanup.
+exec ALL_PROXY="socks5://localhost:1055/" /app/server
