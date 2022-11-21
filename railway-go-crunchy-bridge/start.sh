6d34682bc425b66ba4abf02cd5b694c731d363e9
Dan Croak <dan@statusok.com>
2022-11-20 19:40:56 -0500

    expose 8080

--- railway-go-crunchy-bridge/start.sh
+++ railway-go-crunchy-bridge/start.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 
 # Connect machine to Tailscale tailnet
-/app/tailscaled --tun=userspace-networking --socks5-server=localhost:1055 &
+/app/tailscaled --state=mem: --tun=userspace-networking --socks5-server="localhost:1055" &
 ID="${RAILWAY_GIT_COMMIT_SHA:0:8}"
 /app/tailscale up --authkey="${TAILSCALE_AUTHKEY}" --hostname="railway-app-${ID}"
-/app/tailscale status
 
 # Run server
-/app/server
+ALL_PROXY="socks5://localhost:1055/" /app/server
