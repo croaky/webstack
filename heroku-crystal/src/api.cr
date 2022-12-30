4767b2b72a462b3d8c83bc9dc87277def76d1ebb
Dan Croak <dan@statusok.com>
2022-12-30 12:34:27 -0500

    heroku-crystal: upgrade deps, set default DATABASE_URL

--- heroku-crystal/src/api.cr
+++ heroku-crystal/src/api.cr
@@ -3,7 +3,7 @@ require "kemal"
 require "json"
 require "pg"
 
-DB.open(ENV["DATABASE_URL"]) do |db|
+DB.open(ENV.fetch("DATABASE_URL", "postgres:///webstack_dev")) do |db|
   get "/" do |env|
     db.exec "SELECT 1"
     env.response.content_type = "application/json"
