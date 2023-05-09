b05761ae7019aae64e08889e7b62983701eb1d91
Dan Croak <dan@statusok.com>
2023-05-09 11:52:23 -0400

    fly-go-crunchy-replicas: update Tailscale state
    https://tailscale.com/kb/1132/flydotio/

--- fly-go-crunchy-replicas/start.sh
+++ fly-go-crunchy-replicas/start.sh
@@ -2,7 +2,7 @@
 
 # Connect machine to Tailscale tailnet and auto-remove immediately when machine shuts down.
 # https://tailscale.com/blog/ephemeral-logout/
-/app/tailscaled --state=mem: --socket=/var/run/tailscale/tailscaled.sock &
+/app/tailscaled --state=/var/lib/tailscale/tailscaled.state --socket=/var/run/tailscale/tailscaled.sock &
 ID=$(echo "${FLY_ALLOC_ID}" | cut -d '-' -f 1)
 /app/tailscale up --authkey="${TAILSCALE_AUTHKEY}" --hostname="fly-${FLY_REGION}-${ID}"
 echo "Tailscale started"
