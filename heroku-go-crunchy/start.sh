2e2b4b4b9074b55956aff67b77e3e81e266c9fe5
Dan Croak <dan@statusok.com>
2023-05-31 07:45:31 -0400

    heroku-go-crunchy: revert "replace Tailscale with Fixie"
    This reverts commit c6b3f0e962096c780c7377bf6c539fb6c3cade9e.

---
+++ heroku-go-crunchy/start.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# Connect machine to Tailscale tailnet and auto-remove immediately when machine shuts down.
+# https://tailscale.com/blog/ephemeral-logout/
+/app/tailscaled --tun=userspace-networking --socks5-server=localhost:1055 &
+ID=$(echo "${HEROKU_DYNO_ID}" | cut -d '-' -f 1)
+/app/tailscale up --authkey="${TAILSCALE_AUTHKEY}" --hostname="heroku-${ID}"
+echo "Tailscale started"
+
+# Run server
+ALL_PROXY=socks5://localhost:1055/ /app/server
